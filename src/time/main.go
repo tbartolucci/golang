164de97b750ee9d69e394930279b3cb2a4631577
tbartolucci <tbartolucci@gmail.com>
2017-07-07 07:08:58 -0400

    Time
    simple time package usage and function.

---
+++ src/time/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	hourOfDay := time.Now().Hour()
+	greeting := getGreeting(hourOfDay)
+	fmt.Println(greeting)
+}
+
+func getGreeting(hour int) string {
+	if hour < 12 {
+		return "Good Morning"
+	} else if hour < 18 {
+		return "Good Afternoon"
+	} else {
+		return "Good Evening"
+	}
+}
